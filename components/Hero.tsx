dc3ccb47f17f7f7d45ea2e5000ee7689cac6a91d
Vishoo Verma <vverma971@gmail.com>
2023-12-15 21:35:51 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+        Selling any vehicle, now made easy!
+        </h1>
+
+        <p className="hero__subtitle">
+        Streamline your call selling and buying experience through our effortless listing process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero1.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
